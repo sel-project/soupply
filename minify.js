c1b411ed11f4d9c677b560d25821a002027e804f
Kripth <markwhite1404@gmail.com>
2016-10-17 23:33:24 +0200

    Reorganization

--- minify.js
+++ minify.js
@@ -1,9 +1,12 @@
 
-// minifies a json object
-function minify(json) {
+function creative(json) {
 	var items = json.items;
 	for(var i=0; i<items.length; i++) {
 		if(items[i].id == undefined || items[i].name == undefined) items.splice(i, 1);
 	}
 	return JSON.stringify({"items": items});
+}
+
+function constants(json) {
+	return JSON.stringify({"constants": json.constants});
 }
