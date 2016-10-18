966b4bc1db26d38a9718f639dd1e192b57ba0c6b
Kripth <markwhite1404@gmail.com>
2016-10-18 17:21:31 +0200

    More pocket constants

--- minify.js
+++ minify.js
@@ -1,12 +1,18 @@
 
-function creative(json) {
-	var items = json.items;
-	for(var i=0; i<items.length; i++) {
-		if(items[i].id == undefined || items[i].name == undefined) items.splice(i, 1);
+function minify(json) {
+	function minifyObject(object){
+		// removes elements which key starts with two underscore
+		for(var property in object) {
+			if(object.hasOwnProperty(property)) {
+				if(property.startsWith("__")) {
+					delete object[property];
+				} else if(typeof(object[property]) == "object") {
+					object[property] = minifyObject(object[property]);
+				}
+				//TODO remove empty objects
+			}
+		}
+		return object;
 	}
-	return JSON.stringify({"items": items});
-}
-
-function constants(json) {
-	return JSON.stringify({"constants": json.constants});
+	return JSON.stringify(minifyObject(json));
 }
