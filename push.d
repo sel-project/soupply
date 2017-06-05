6ed2fc9235698b4c1980eefc7c5d51c837fa5413
Kripth <markwhite1404@gmail.com>
2017-06-05 17:01:52 +0200

    Use a branch for utils

--- push.d
+++ push.d
@@ -118,19 +118,16 @@ void main(string[] args) {
 				wait(spawnShell("cd " ~ lang ~ " && git checkout --orphan " ~ branch));
 			} else {
 				// checkout existing branch and update it
-				writeln("Branch ", branch, " already exists, pulling files...");
 				wait(spawnShell("cd " ~ lang ~ " && git pull"));
 			}
 			// delete all files but .git
 			executeShell("cd " ~ lang ~ " && find . -type f -not -wholename '*.git*' -print0 | xargs -0 rm --");
-			// copy .editorconfig
-			//if(ec.length) write(lang ~ "/.editorconfig", ec);
 			// copy only files that has 'match' in the name
 			auto regex_file = regex("(" ~ branch ~ ")" ~ protocol, "mi");
 			auto regex_content = regex("(" ~branch ~ ")" ~ protocol ~ "((?!\\.xml))", "mi");
-			foreach(string file ; dirEntries("src/" ~ lang ~ "/sul/", SpanMode.breadth)) {
-				if(file.isFile && (file.toLower.indexOf(match) != -1 || file.toLower.indexOf("utils/") != -1)) {
-					string dest = file[("src/" ~ lang ~ "/sul/").length..$];
+			foreach(string file ; dirEntries("src/" ~ lang, SpanMode.breadth)) {
+				if(file.isFile && file.toLower.indexOf(match) != -1) {
+					string dest = file[("src/" ~ lang).length+1..$];
 					if(protocol.length) dest = dest.replaceAll(regex_file, "$1");
 					if(dest.indexOf("/") != -1) mkdirRecurse(lang ~ "/" ~ dest[0..dest.lastIndexOf("/")]);
 					string content = cast(string)read(file);
@@ -151,6 +148,7 @@ void main(string[] args) {
 			}
 			checkout(game, game ~ data["latest"].integer.to!string);
 		}
+		checkout("utils", "utils");
 		
 	}
 
